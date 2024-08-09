7c149b70da9dffc86d6c647c2937824a9652e9d1
Vikas Sharma <vs509411@gmail.com>
2024-08-09 15:11:44 +0530

    Create Employee api created

--- src/main/java/com/payroll/employee/service/backend/controller/EmployeeController.java
+++ src/main/java/com/payroll/employee/service/backend/controller/EmployeeController.java
@@ -1,13 +1,14 @@
 package com.payroll.employee.service.backend.controller;
 
+import com.payroll.employee.service.backend.dto.EmployeeCreateDto;
 import com.payroll.employee.service.backend.dto.EmployeeDto;
 import com.payroll.employee.service.backend.service.EmployeeService;
+import lombok.extern.flogger.Flogger;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -18,8 +19,8 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping
-    private ResponseEntity<EmployeeDto> getEmployeeById(Long employeeId)
+    @GetMapping("/{id}")
+    private ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long employeeId)
     {
         Optional<EmployeeDto> employeeDto = employeeService.getEmployeeById(employeeId);
 
@@ -27,6 +28,13 @@ public class EmployeeController {
                 .map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))  // If present, return 200 OK with the EmployeeDto
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));  // If not present, return 404 Not Found
     }
+
+    @PostMapping()
+    private ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeCreateDto employeeCreateDto){
+        EmployeeDto employeeDto=employeeService.createEmployee(employeeCreateDto);
+        return new ResponseEntity<>(employeeDto,HttpStatus.CREATED);
+    }
+
 
 
 
--- src/main/java/com/payroll/employee/service/backend/repository/EmployeeRepository.java
+++ src/main/java/com/payroll/employee/service/backend/repository/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.payroll.employee.service.backend.repository;
 
 import com.payroll.employee.service.backend.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
 
+    @Query(value = "SELECT e.employee_id FROM employee_detail e ORDER BY e.employee_id DESC LIMIT 1", nativeQuery = true)
+    Long findMaxEmployeeId();
 }
