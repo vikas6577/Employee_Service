36e97282fcd77721237b548ed7db22e5f6f9af82
jayant26 <jayantjain2609@gmail.com>
2024-08-19 11:55:45 +0530

    subtle changes

--- src/main/java/com/payroll/employee/service/backend/Application.java
+++ src/main/java/com/payroll/employee/service/backend/Application.java
@@ -7,10 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
